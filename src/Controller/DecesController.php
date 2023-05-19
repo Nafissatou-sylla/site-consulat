1c929998c29df7e987d9772b56dc18781c2ba0c6
Nafissatou SYLLA <nafissatou.sylla98@gmail.com>
2023-05-19 13:08:24 +0200

    page pour les décés

---
+++ src/Controller/DecesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DecesController extends AbstractController
+{
+    #[Route('/deces', name: 'app_deces')]
+    public function index(): Response
+    {
+        return $this->render('deces/index.html.twig', [
+            'controller_name' => 'DecesController',
+        ]);
+    }
+}
