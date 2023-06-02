9713f990c96f592b24083c49343fef7c55ce6a2a
Nafissatou SYLLA <nafissatou.sylla98@gmail.com>
2023-06-02 13:17:27 +0200

    personnaliser le formulaire avec bootstrap

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -29,6 +30,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(message:"le nom est obligatoire")]
     private ?string $nom = null;
 
     #[ORM\Column(length: 100)]
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,20 +8,22 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Form\AddresseType;
 use App\Form\FormationType;
-
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
             ->add('telephone')
-            ->add('dateDeNaissance')
+            ->add('dateDeNaissance', DateType::class,)
             ->add('lieuDeNaissance')
             ->add('paysDeNaissance')
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('password')     
             ->add('adresse',AddresseType::class) 
             ->add('formation',FormationType::class)   
